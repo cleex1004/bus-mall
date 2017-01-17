936e5374a620f1fca86cf77099a2589b23f5fc44
cleex1004 <cleex1004@gmail.com>
2017-01-17 14:45:46 -0800

    changed jpg name and started form

--- app.js
+++ app.js
@@ -1,42 +1,62 @@
 'use strict';
-var displayed = [];
+//global variables
+var numberArray = [];
+var pictureArray = [];
+var numberDisplayed = [];
+var pictureDisplayed = [];
 
-function Product(number, name, picture) {
+//product constructor
+function Product(number, description, picture) {
   this.number = number;
-  this.name = name;
+  this.name = description;
   this.picture = picture;
   this.shown = [];
   this.clicked = [];
+  numberArray.push(this.number);
+  pictureArray.push(this.picture);
 };
 
+//random number function
 function randomNumber() {
   var min = Math.ceil(1);
   var max = Math.floor(20);
-  for (var i = 0; i < 3; i++) {
-    displayed.push(Math.floor(Math.random() * (max - min)) + min);
+  for (var u = 0; u < 3; u++) {
+    numberDisplayed.push(numberArray[Math.floor(Math.random() * (max - min)) + min]);
+    pictureDisplayed.push(pictureArray[Math.floor(Math.random() * (max - min)) + min]);
   };
-  return Math.floor(Math.random() * (max - min)) + min;
 };
 
-var bag = new Product(1, 'Starwars Suitcase', bag.jpg);
-var banana = new Product(2, 'Banana Slicer', banana.jpg);
-var bathroom = new Product(3, 'Ipad Stand', bathroom.jpg);
-var boots = new Product(4, 'Toeless Rain Boots', boots.jpg);
-var breakfast = new Product(5, 'All-in-one Breakfast Maker', breakfast.jpg);
-var bubblegum = new Product(6, 'Meatball Bubblegum', bubblegum.jpg);
-var chair = new Product(7, 'Red Chair', chair.jpg);
-var cthulhu = new Product(8, 'Action Figure', cthulhu.jpg);
-var dogduck = new Product(9, 'Duck Bill for Dog', dogduck.jpg);
-var dragon = new Product(10, 'Dragon Meat Can', dragon.jpg);
-var pen = new Product(11, 'Utensil Capped Pen', pen.jpg);
-var petsweep = new Product(12, 'Sweeping Slippers for Pet', petsweep.jpg);
-var scissors = new Product(13, 'Pizza Slice Scissors', scissors.jpg);
-var shark = new Product(14, 'Shark Sleeping Bag', shark.jpg);
-var sweep = new Product(15, 'Baby Sweeper Onesie', sweep.jpg);
-var tauntaun = new Product(16, 'Tauntaun Sleeping Bag', tauntaun.jpg);
-var unicorn = new Product(17, 'Unicorn Meat Can', unicorn.jpg);
-var usb = new Product(18, 'Tentacle Usb', usb.gif);
-var watercan = new Product(19, 'Watering Can', watercan.jpg);
-var wineglass = new Product(20, 'Wine Glass', wineglass.jpg);
+function display() {
+  var legendEl = document.getElementById('here');
+  for (var v = 0; v < 3; v++) {
+    var inputEl = document.createElement('input');
+    inputEl.setAttribute('type', 'image');
+    inputEl.setAttribute('src', '/img/' + pictureDisplayed[v] + '.jpg');
+    legendEl.appendChild(inputEl);
+  }
+};
+
+//create all products
+var bag = new Product(1, 'Starwars Suitcase', 'bag');
+var banana = new Product(2, 'Banana Slicer', 'banana');
+var bathroom = new Product(3, 'Ipad Stand', 'bathroom');
+var boots = new Product(4, 'Toeless Rain Boots', 'boots');
+var breakfast = new Product(5, 'All-in-one Breakfast Maker', 'breakfast');
+var bubblegum = new Product(6, 'Meatball Bubblegum', 'bubblegum');
+var chair = new Product(7, 'Red Chair', 'chair');
+var cthulhu = new Product(8, 'Action Figure', 'cthulhu');
+var dogduck = new Product(9, 'Duck Bill for Dog', 'dogduck');
+var dragon = new Product(10, 'Dragon Meat Can', 'dragon');
+var pen = new Product(11, 'Utensil Capped Pen', 'pen');
+var petsweep = new Product(12, 'Sweeping Slippers for Pet', 'petsweep');
+var scissors = new Product(13, 'Pizza Slice Scissors', 'scissors');
+var shark = new Product(14, 'Shark Sleeping Bag', 'shark');
+var sweep = new Product(15, 'Baby Sweeper Onesie', 'sweep');
+var tauntaun = new Product(16, 'Tauntaun Sleeping Bag', 'tauntaun');
+var unicorn = new Product(17, 'Unicorn Meat Can', 'unicorn');
+var usb = new Product(18, 'Tentacle Usb', 'usb');
+var watercan = new Product(19, 'Watering Can', 'watercan');
+var wineglass = new Product(20, 'Wine Glass', 'wineglass');
 
 randomNumber();
+display();
